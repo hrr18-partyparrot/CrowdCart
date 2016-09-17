e53f0dfb2da2c6b6892a888921c536acb83e0a86
Tpredale <tpredale@gmail.com>
2016-09-17 15:15:00 -0400

    Testing livechat functionality

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// require app, mongoose
-var app = require('./server/server.js');
-var mongoose = require('mongoose');
+// // require app, mongoose
+// var app = require('./server/server.js');
+// var mongoose = require('mongoose');
 
-// set mongoURI
-var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/crowdcart';
+// // set mongoURI
+// var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/crowdcart';
 
-// connect db
-mongoose.connect(mongoURI);
+// // connect db
+// mongoose.connect(mongoURI);
 
-// set port
-var port = process.env.PORT || 1337;
+// // set port
+// var port = process.env.PORT || 1337;
 
-// listen on port
-app.listen(port);
+// // listen on port
+// http.listen(port);
 
-console.log("Server is listening on port " + port);
+// console.log("Server is listening on port " + port);
 
